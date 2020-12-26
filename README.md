2347951f4bed324ad334013a32770528c004ea1c
Vlad Lisovskii <vladislav.lisovskii@bonial.com>
2020-12-26 14:55:01 +0100

    Add simple DO droplet creation and README with goal of the training and commands to start

---
+++ README.md
@@ -0,0 +1,13 @@
+# Terraform applying to Digital Ocean
+
+*(just another give a try personal self-training)*
+
+## Requirements:
+
+* I use latest terraform:
+```
+$ terraform --version
+
+Terraform v0.14.3
+```
+* Init modules: `terraform init`
--- main.tf
+++ main.tf
@@ -6,3 +6,18 @@ terraform {
     }
   }
 }
+
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a web server
+resource "digitalocean_droplet" "droplet-test" {
+  image  = "ubuntu-18-04-x64"
+  name   = "terraform-droplet-test"
+  region = "ams3"
+  size   = "s-1vcpu-1gb"
+}
